c7d140ac9e86e6560b88283e348706a18375347e
Shiv Matta <shiv.matta@gmail.com>
2022-06-07 17:31:20 -0500

    commit tree 5e277dbadd56e3311481d5776250cea0bff6b739

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue < 400) {
+    return 'This one is on me!'
+  } else if (someValue >= 400 && someValue<= 2000){
+    return 'That will be twenty bucks.'
+  } else if (someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someValue > 2500) {
+    return 'No can do.'
+  }
+  return result
+  }
+
+
+
+function ternaryCheckCity(cityName){
+return cityName === 'NYC'?"Ok, sounds good.": "No go."
+
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tipAmt){
+ switch(tipAmt){
+   case 'generous':
+   return "Thank you so much."
+   case 'not as generous':
+     return 'Thank you.'
+  default:
+    return 'Bye.'
+ }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
